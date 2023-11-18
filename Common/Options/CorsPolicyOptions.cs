3bdd4259e43cbfb8aedb24c50b0b5f85b87d96cf
chioroglo <kioroglomobster@gmail.com>
2023-11-18 16:44:45 +0200

    tweak

--- Common/Options/CorsPolicyOptions.cs
+++ Common/Options/CorsPolicyOptions.cs
@@ -3,6 +3,6 @@ namespace Common.Options;
 public class CorsPolicyOptions : IApplicationOptions
 {
     public static string Config => "CorsPolicyOptions";
-    public string[] AllowedOrigins { get; init; } = new string[] { };
-    public string[] AllowedMethods { get; init; } = new string[] { };
+    public string[] AllowedOrigins { get; init; } = Array.Empty<string>();
+    public string[] AllowedMethods { get; init; } = Array.Empty<string>();
 }
