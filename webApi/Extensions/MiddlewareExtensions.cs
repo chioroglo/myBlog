3fd0ffdbf8ea82ae6b1cc4b6269b11973e3df247
chioroglo <kioroglomobster@gmail.com>
2024-10-19 01:10:47 +0300

    exception handling middleware error

--- webApi/Extensions/MiddlewareExtensions.cs
+++ webApi/Extensions/MiddlewareExtensions.cs
@@ -4,10 +4,6 @@ namespace API.Extensions
 {
     public static class MiddlewareExtensions
     {
-        public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app)
-        {
-            return app.UseMiddleware<ExceptionHandlingMiddleware>();
-        }
 
         public static IApplicationBuilder UseDatabaseTransactions(this IApplicationBuilder app)
         {
