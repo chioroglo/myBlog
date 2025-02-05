6fe2221c5c934a26fece338fd5878b9f4f0b13f5
Alexandr Chioroglo <kioroglomobster@gmail.com>
2025-02-05 22:49:35 +0200

    base e2e functional tests setup

--- MyBlog.Common/Models/UserModel.cs
+++ MyBlog.Common/Models/UserModel.cs
@@ -5,9 +5,7 @@ namespace MyBlog.Common.Models
     public class UserModel : BaseModel
     {
         public string Username { get; set; }
-
         public string FullName { get; set; }
-
         public string LastActivity { get; set; }
         public string Initials { get; set; }
         public bool IsBanned { get; set; }
---
+++ MyBlog.FunctionalTests/Utils/ContainersSetup.cs
@@ -0,0 +1,11 @@
+﻿using Testcontainers.MsSql;
+
+namespace MyBlog.FunctionalTests.Utils;
+
+public static class ContainersSetup
+{
+    public static MsSqlContainer BuildMsSqlContainer() => new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithPassword("P@ssword!")
+        .Build();
+}
