fc9fb6bca15385ab2d22a7c51b47ee97be45161b
Alexandr Chioroglo <kioroglomobster@gmail.com>
2025-02-18 23:45:12 +0200

    ef9 test migration

--- MyBlog.Data/Migrations/20250218185509_Ef9Update.Designer.cs
+++ MyBlog.Data/Migrations/20250218185509_Ef9Update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyBlog.Data;
 
@@ -11,9 +12,11 @@ using MyBlog.Data;
 namespace MyBlog.Data.Migrations
 {
     [DbContext(typeof(BlogDbContext))]
-    partial class BlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250218185509_Ef9Update")]
+    partial class Ef9Update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
